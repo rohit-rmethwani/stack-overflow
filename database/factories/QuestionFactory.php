5a89a0b1581fb8a175568e8eb35d3e39efa60c5c
Rohit Methwani <rohit.methwan31@gmail.com>
2020-04-09 14:14:45 +0530

    Updated answer and question factory

--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -8,8 +8,6 @@ use Faker\Generator as Faker;
 $factory->define(Question::class, function (Faker $faker) {
     return [
         'title' => rtrim($faker->sentence(rand(5,10)), '.'),
-        'body' => $faker->paragraphs(rand(3, 7), true),
-        'views_count' => rand(0, 10),
-        'votes_count' => rand(-10, 10)
+        'body' => $faker->paragraphs(rand(3, 7), true)
     ];
 });
