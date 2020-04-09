f04a253e2a36ec028502e8bf3cb2be4089f6d9db
Rohit Methwani <rohit.methwan31@gmail.com>
2020-04-09 14:06:07 +0530

    Mapped relationship on Questions Table

--- app/Question.php
+++ app/Question.php
@@ -71,4 +71,26 @@ class Question extends BaseModel
     public function votes(){
         return $this->morphToMany(User::class, 'vote')->withTimestamps();
     }
+
+    public function vote(int $vote){
+        $this->votes()->attach(auth()->id(),['vote'=>$vote]);
+        if($vote < 0){
+            $this->decrement('votes_count');
+        }
+        else{
+            $this->increment('votes_count');
+        }
+    }
+
+    public function updateVote(int $vote){
+        $this->votes()->updateExistingPivot(auth()->id(), ['vote'=>$vote]);
+        if($vote < 0){
+            $this->decrement('votes_count');
+            $this->decrement('votes_count');
+        }
+        else{
+            $this->increment('votes_count');
+            $this->increment('votes_count');
+        }
+    }
 }
